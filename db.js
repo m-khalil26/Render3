3b6da3989b8dd7a78ac068b0e7e00f1b23f3a362
makhlkh2 <khalil.makhloufi@univ-rouen.fr>
2023-11-22 12:59:03 +0100

    version fonct normalement

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database url
+  process.env.DATABASE_URL, // TODO: database url
   {
     dialect: 'postgres',
     dialectOptions: {
